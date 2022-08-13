e97411b7d7042363d7b2190fbe135016130b9079
Zhanna March <marzhanter@gmail.com>
2022-08-13 23:30:46 +0300

    -m add-selenide2-HomeWork

---
+++ src/test/java/tests/HomeworkSelenide2Test.java
@@ -0,0 +1,26 @@
+package tests;
+
+import com.codeborne.selenide.*;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.interactions.Actions;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class HomeworkSelenide2Test {
+
+    @Test
+    void hoverTest() {
+        open("https://github.com");
+        Selenide.clearBrowserLocalStorage();
+        $(".HeaderMenu").$(Selectors.byText("Pricing")).hover();
+        $(Selectors.byText("Compare plans")).click();
+    }
+    @Test
+    void dragAndDropTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        SelenideElement a = $("#column-a");
+        SelenideElement b = $("#column-b");
+        a.dragAndDropTo(b); //it works
+        actions().dragAndDrop(b, a); //it doesn't work
+    }
+}
