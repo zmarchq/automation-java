391b77350aa8082c188904ee65a3713b483d95ed
Zhanna March <marzhanter@gmail.com>
2022-09-21 21:41:10 +0300

    -m create-allure-parametrized-test

--- src/test/java/tests/PropertyTest.java
+++ src/test/java/tests/PropertyTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 public class PropertyTest {
 
     @Test
-    @Tag("property")
     void parametersTest() {
         String name = System.getProperty("browser_size");
         System.out.println(name);
